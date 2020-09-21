2408162c2755c99d81ca28fd22af39fcffc7a9d9
Oleg Karataev <51063299+CrazyPhD@users.noreply.github.com>
2020-09-21 04:09:05 +0000

    1_9.sh suppress errors

--- lab1/1_9.sh
+++ lab1/1_9.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find /var/log/* -name '*.log' | xargs wc -l | sort -nr | head -n 1 | awk '{print "Total lines count:", $1}'
+find /var/log/* -name '*.log' 2>/dev/null | xargs wc -l 2>/dev/null | sort -nr | head -n 1 | awk '{print "Total lines count:", $1}'
