8edf8f78c765ecd76b0982fe8b1e425d824fc021
Oleg Karataev <51063299+CrazyPhD@users.noreply.github.com>
2020-09-21 04:02:09 +0000

    1_7.sh suppress errors flag added

--- lab1/1_7.sh
+++ lab1/1_7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 email_pattern="[a-zA-Z0-9._]\+@[a-zA-Z0-9.]\+\.[a-zA-Z0-9.]\+"
-email_list=$(grep -Roh $email_pattern /etc | grep -o $email_pattern)
+email_list=$(grep -Rohs $email_pattern /etc | grep -o $email_pattern)
 echo $email_list | sed 's/ /, /g' > emails.lst
