d12a98eed25486f368b12347c99a79025962aa3c
CrazyPhD <karataev.oleg2010@yandex.ru>
2020-10-05 13:28:46 +0000

    More correct solution

--- lab2/2_2.sh
+++ lab2/2_2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps -Ao pid,command | tail -n +2 | grep "/sbin/" | awk '{print $1}' > 2_2.txt
+ps -Ao pid,command | tail -n +2 | awk '{if ($2 ~ /^\/sbin\//)print $1}' > 2_2.txt
--- lab2/2_7.sh
+++ lab2/2_7.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-ps aux | awk '{if ($6 != 0) print $2" "$6" "$11}' | sort -nk1 | tail -n +2 | head -n -6 > 2_7_1.ps
+check() {
+for x in $(ps -Ao pid,command | tail -n +2 | awk '{print $1":"$2}'); do
+	pid=$(echo $x | awk -F ":" '{print $1}')
+	cmd=$(echo $x | awk -F ":" '{print $2}')
+	f="/proc/"$pid
+	if [ -f $f/io ]; then
+		rbytes=$(grep -h "read_bytes:" $f/io | sed "s/[^0-9]*//")
+		echo "$pid $cmd $rbytes"
+	fi
+done | sort -nk1
+}
+
+check > 2_7_1.ps
 sleep 1m
-ps aux | awk '{if ($6 != 0) print $2" "$6" "$11}' | sort -nk1 | tail -n +2 | head -n -6 > 2_7_2.ps
+check > 2_7_2.ps
 
 cat 2_7_1.ps |
 while read str; do
-	pid=$(awk '{print $1}' <<< $str)
-	mem0=$(awk '{print $2}' <<< $str)
-	cmd=$(awk '{print $3}' <<< $str)
+        pid=$(awk '{print $1}' <<< $str)
+        mem0=$(awk '{print $3}' <<< $str)
+        cmd=$(awk '{print $2}' <<< $str)
 
-	mem1=$(cat 2_7_2.ps | awk -v id="$pid" '{if ($1 == id) print $2}')
-	memdiff=$(echo "$mem1-$mem0" | bc)
-	echo $pid":"$cmd":"$memdiff
+        mem1=$(cat 2_7_2.ps | awk -v id="$pid" '{if ($1 == id) print $3}')
+        memdiff=$(echo "$mem1-$mem0" | bc)
+        echo $pid":"$cmd":"$memdiff
 done | sort -t ':' -nrk3 | head -3
 
 rm *.ps
